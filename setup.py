e5adae7a573fa378f599d5918f75080986fe87f4
Maxim Novikov <nes8bit@yandex-team.ru>
2016-10-20 16:31:31 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+
+ext1 = Extension(
+    'spatial_index', ['spatial.cpp'],
+    libraries=['boost_python'],
+    extra_compile_args=['-std=c++11', '-g'],
+)
+
+
+setup(
+    name='spatial_index',
+    version='0.0.1',
+    author='Maxim Novikov',
+    author_email='nes.8bit@yandex.ru',
+    license='MIT',
+    install_requires=[''],
+    classifiers=['Development Status :: 3 - Alpha'],
+    ext_modules=[ext1],
+)
